e0cdedca4e71c70c040b09e28d2e92e376bc229b
Belmouk <el.mehdi.boukhiam@gmail.com>
2025-10-31 14:40:41 +0100

    fix: handle consecutive operations and update CE and AC logic

--- script.js
+++ script.js
@@ -32,6 +32,12 @@ const generateExpression = function (userInputs) {
     let expression =  { operand1: "", operator: "", operand2: ""}
     const OPERATORS = { "+": null, "-": null, "/": null, "*": null};
     let role = "operand1";
+    
+    if (userInputs.length === 1) {
+        return {operand1: userInputs[0], operator: "*", operand2: "1"};
+    } else if (userInputs.length === 0) {
+        return {operand1: "0", operator: "*", operand2: "0"};
+    }
 
     for (input of userInputs) {
         if (!(input in OPERATORS)) {
@@ -40,10 +46,10 @@ const generateExpression = function (userInputs) {
             expression["operator"] = input;
             role = "operand2";
         }
-    };
+    }
 
     return expression;
-}
+};
 
 const runCalculator = function () {
     // add click listener to the inputs container
@@ -51,48 +57,71 @@ const runCalculator = function () {
     const resultDisplay = document.querySelector("#result");
     const expressionDisplay = document.querySelector("#expression");
     let i = 0;
-    let expression = ["", "", ""];
+    let userInputs = [];
     const NUMBERS = {0: null, 1: null, 2: null, 3: null, 4: null, 5: null, 6: null, 7: null, 8: null, 9: null};
     const OPERATORS = { "+": null, "-": null, "/": null, "*": null};
+    let result = 0;
 
     calculator.addEventListener("click", (e) => {
         const input = e.target;
-
-        if (input.type === "button") {
-            if (input.textContent === "CE") {
-                resultDisplay.textContent = "0";
-            } else if (input.textContent === "AC") {
-                i = 0;
-                expression = ["", "", ""];
-                expressionDisplay.textContent = "";
-                resultDisplay.textContent = "0";
-            }
-            
-            else {
-                    resultDisplay.textContent = input.textContent;
-                    if (input.textContent in NUMBERS) {
-                    expression[i] += input.textContent;
-                    expressionDisplay.textContent = expression.join("");
-                } else if (input.textContent in OPERATORS) {
-                    expression[1] = input.textContent;
-                    i = 2;
-                    expressionDisplay.textContent = expression.join("");
-                } else if (input.textContent === "=") {
-                    i = 0;
-                    const evaluation = evaluateExpression(expression);
-                    expressionDisplay.textContent = expression.join("") + "=";
-                    expression = ["", "", ""]
-                    expression[0] = evaluation;
-                    resultDisplay.textContent = evaluation;
-                }
-
-                console.log(input);
-                console.log(input.textContent);
-                console.log(expression);
+        
+        if (input.textContent in NUMBERS) {
+            userInputs.push(input.textContent);
+        } else if (input.textContent in OPERATORS) {
+            if (userInputs.filter(input => input in OPERATORS).length === 1) {
+                const interimResult = evaluateExpression(generateExpression(userInputs));
+                userInputs = [];
+                userInputs.push(interimResult);
             } 
+            userInputs.push(input.textContent);
+        } else if (input.textContent === "CE") {
+            userInputs.pop();
+        } else if (input.textContent === "AC") {
+            userInputs = []
+        } else if (input.textContent === "=") {
+            result = evaluateExpression(generateExpression(userInputs));
+            userInputs = [result.toString()];
+        }
+        console.log(input);
+        console.log(userInputs);
+        console.log(result);
+
+
+        // if (input.type === "button") {
+        //     if (input.textContent === "CE") {
+        //         resultDisplay.textContent = "0";
+        //     } else if (input.textContent === "AC") {
+        //         i = 0;
+        //         expression = ["", "", ""];
+        //         expressionDisplay.textContent = "";
+        //         resultDisplay.textContent = "0";
+        //     }
+            
+        //     else {
+        //             resultDisplay.textContent = input.textContent;
+        //             if (input.textContent in NUMBERS) {
+        //             expression[i] += input.textContent;
+        //             expressionDisplay.textContent = expression.join("");
+        //         } else if (input.textContent in OPERATORS) {
+        //             expression[1] = input.textContent;
+        //             i = 2;
+        //             expressionDisplay.textContent = expression.join("");
+        //         } else if (input.textContent === "=") {
+        //             i = 0;
+        //             const evaluation = evaluateExpression(expression);
+        //             expressionDisplay.textContent = expression.join("") + "=";
+        //             expression = ["", "", ""]
+        //             expression[0] = evaluation;
+        //             resultDisplay.textContent = evaluation;
+        //         }
+
+        //         console.log(input);
+        //         console.log(input.textContent);
+        //         console.log(expression);
+        //     } 
         
 
-        }
+        // }
 
     })
 }
